dbd6dc5f113a4617a457e519aefaa5c25b1cb675
Isaiah Rappaport <irappap98@gmail.com>
2020-10-29 22:28:16 -0400

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link> 
       <router-link to="/about">About</router-link>
+      <router-link to="/signup">Signup</router-link>
+      <router-link to="/login">Login</router-link>
+      <router-link to="/logout">Logout</router-link>
     </div>
     <router-view/>
   </div>
@@ -24,6 +27,7 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  padding: 30px;
 }
 
 #nav a.router-link-exact-active {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- indexSongs -->
+    <h1>All Songs:</h1>
+    <div v-for="song in songs">
+      <h2>{{song.title}}</h2>
+      <h3>By: {{song.user_id}}</h3>
+      <h4>Audio: {{song.audio}}</h4>
+      <p>Lyrics: {{song.lyrics}}</p>
+      <button v-on:click="showSong(song)">Listen</button>
+      <br>
+      <br>
+      <br>
+    </div>
+
+<!-- showSong -->
+      <dialog id="song-details">
+      <form method="dialog">
+        <h2>Song info:</h2>
+        <h3>{{ currentSong.title }}</h3>
+        <p> {{ currentSong.audio }}</p>
+        <p> {{ currentSong.lyrics }}</p>
+        <p> {{ currentSong.suggestions }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
+
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      songs: [],
+      currentSong: {},
+    };
+  },
+  created: function () {
+    this.indexSongs();
+  },
+  methods: {
+    // indexSongs
+    indexSongs: function () {
+      axios.get("/api/songs").then((response) => {
+        console.log("All songs:", response.data);
+        this.songs = response.data;
+      });
+    },
+    // showSong
+    showSong: function (song) {
+      console.log("Show song:", song);
+      this.currentSong = song;
+      document.querySelector("#song-details").showModal();
+    },
+  },
+};
 </script>
